03da994be177dbaa0ba794af8719b946e545b487
punctual-sailor-2214 <bchett286@yahoo.com>
2019-04-03 19:17:24 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,28 @@ const codes = [
   "a"
 ];
 
+
+
 function init() {
-  // your code here
-}
+
+    var index = 0;
+    document.body.addEventListener("keydown", onKeyDownHandler);
+
+    function onKeyDownHandler(e) {
+
+      const key = e.key;
+
+      if (key === codes[index]) {
+        index++;
+
+        if (index === codes.length) {
+          alert("CONGRATS!!!");
+          index = 0;
+        }
+      } else {
+        index = 0;
+      }
+    }
+  }
+
+init();
